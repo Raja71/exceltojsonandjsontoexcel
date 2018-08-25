3a3b61b1532888c42a068ebbfd80f9a3b0c71f5c
Ganesh <ganeshmk100@gmail.com>
2018-08-25 10:26:45 +0530

    file convert to JSON

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,44 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx'
+
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
+
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
-excel(){
-  // alert(event.target.id);
-  var testUrl= "../assets/Sample_data1.xlsx";
-  var oReq = new XMLHttpRequest();
-  oReq.open("GET", testUrl, true);
-  oReq.responseType = "arraybuffer";
-  
-  oReq.onload = function(e) {
-    var arraybuffer = oReq.response;
-  
-    /* convert data to binary string */
-    var data = new Uint8Array(arraybuffer);
-    var arr = new Array();
+  constructor() {}
+
+  excel(event){
+    var input: any = document.getElementById("files")
+    var files = input.files;
+    var reader = new FileReader();
+    var fileData = new Blob([files[0]]);
+    reader.readAsArrayBuffer(fileData);
+    reader.onload = function() {
+      var arrayBuffer = reader.result
+      var data = new Uint8Array(arrayBuffer);
+      var arr = new Array();
     for(var i = 0; i != data.length; ++i){ 
       arr[i] = String.fromCharCode(data[i]);
-      //  console.log("Data"+data[i]);
     }
     var bstr = arr.join("");
     var workbook = XLSX.read(bstr, {type:"binary"});
-  //console.log("Data"+bstr);
-   var first_sheet_name = workbook.SheetNames[0];
-    /* Get worksheet */
+    var first_sheet_name = workbook.SheetNames[0];
     var worksheet = workbook.Sheets[first_sheet_name]; 
     var json = XLSX.utils.sheet_to_json(worksheet);
-    // workbook.Sheets[workbook.SheetNames[0]], {header:1, raw:true}
     var jsonOut = JSON.stringify(json);
-     console.log("test"+jsonOut);
+    console.log("test"+jsonOut);
+    }
   }
-  oReq.send();
-  }
-  ngOnInit() {
-    this.excel();
-  }
+  ngOnInit() {}
 }
