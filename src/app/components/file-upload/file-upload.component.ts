9fefdebe44013f4c8684724153b7fe12c35b35f5
Kameshwari@tapontech.com <Kameshwari@tapontech.com>
2018-08-25 12:07:59 +0530

    changes

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -10,9 +10,10 @@ import * as XLSX from 'xlsx'
 export class FileUploadComponent implements OnInit {
 
   constructor() {}
-
   excel(event){
-    let input: any = document.getElementById("files")
+    let ids=['samplefile','globalmarket'];
+    for(var i = 0; i < ids.length; i++) {
+    let input: any = document.getElementById(ids[i])
     let files = input.files;
     let reader = new FileReader();
     let fileData = new Blob([files[0]]);
@@ -31,29 +32,8 @@ export class FileUploadComponent implements OnInit {
     let json = XLSX.utils.sheet_to_json(worksheet);
     let jsonOut = JSON.stringify(json);
     console.log("test"+jsonOut);
-    }
-  }
-  exec(event){
-    let input: any = document.getElementById("files1")
-    let files = input.files;
-    let reader = new FileReader();
-    let fileData = new Blob([files[0]]);
-    reader.readAsArrayBuffer(fileData);
-    reader.onload = function() {
-      let arrayBuffer = reader.result
-      let data = new Uint8Array(arrayBuffer);
-      let arr = [];
-    for(let i = 0; i != data.length; ++i){ 
-      arr[i] = String.fromCharCode(data[i]);
-    }
-    let bstr = arr.join("");
-    let workbook = XLSX.read(bstr, {type:"binary"});
-    let first_sheet_name = workbook.SheetNames[0];
-    let worksheet = workbook.Sheets[first_sheet_name]; 
-    let json = XLSX.utils.sheet_to_json(worksheet);
-    let jsonOut = JSON.stringify(json);
-    console.log("test"+jsonOut);
-    }
-  }
+   }
+ }
+}
   ngOnInit() {}
 }
